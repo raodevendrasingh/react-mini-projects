2f4e25f2add4d59f49c902481d86603f17d7566d
Devendra Singh Rao <dsrao1602@gmail.com>
2023-11-25 21:42:17 +0530

    started routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import "./App.css";
-import BgChanger from "./components/BgChanger";
-import PasswordGenerator from "./components/PasswordGenerator";
-import CurrencyConverter from "./components/CurrencyConverter";
+// import BgChanger from "./components/BgChanger";
+// import PasswordGenerator from "./components/PasswordGenerator";
+// import CurrencyConverter from "./components/CurrencyConverter";
+import Main from "./components/RouterProject/Main/Main";
 
 const App = () => {
 	return (
 		<>
-			<h1 className="text-center text-4xl text-blue-400 py-10">
+			{/* <h1 className="text-center text-4xl text-blue-400 py-10">
 				React Projects
 			</h1>
 			<div className=" flex justify-center text-xl text-gray-100">
@@ -16,7 +17,9 @@ const App = () => {
 					<li>Password Generator</li>
 					<li>Currency Convertor</li>
 				</ol>
-			</div>
+			</div> */}
+
+            <Main/>
 
 			{/* <BgChanger /> */}
 			{/* <PasswordGenerator /> */}
---
+++ src/components/RouterProject/Main/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import Home from "../Content/Home";
+
+const Main = () => {
+	return (
+		<>
+			<Header />
+			<Home />
+			<Footer />
+		</>
+	);
+};
+
+export default Main
